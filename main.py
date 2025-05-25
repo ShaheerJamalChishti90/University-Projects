771f4a2a8379c2bf6f501838c99ddacb9b6ff576
ShaheerJamalChishti09 <shaheerjamal369@gmail.com>
2025-05-25 17:31:32 +0500

    University Projects

---
+++ main.py
@@ -0,0 +1,15 @@
+# class Account:
+#     def __init__(self, account_holdername, account_number, balance=0):
+#         self.account_holdername = account_holdername
+#         self.account_number = account_number
+#         self.balance = balance
+        
+#     def deposit(self, amount):
+#         self.balance += amount # balance = balance + amount
+#         return f"Current Balance: {self.balance}"
+
+x = ((3 + 4) * (4 + 5) * (4 + 3) - 1)
+print(x)
+
+# 8
+# 20
